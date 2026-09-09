cde4cb458c5a0cb8be8afb567e758d81993bc3d8
Arturo Cachon <cachonto@ualberta.ca>
2026-09-09 17:44:32 -0600

    CityList

---
+++ src/main/java/com/example/citylist/City.kt
@@ -0,0 +1,23 @@
+package com.example.citylist
+
+import androidx.compose.runtime.mutableStateListOf
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        "Edmonton", "Vancouver", "Moscow",
+        "Sydney", "Berlin", "Vienna",
+        "Tokyo", "Beijing", "Osaka",
+        "New Delhi"
+    )
+
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String){
+        _cities.add(city)
+    }
+
+    fun removeCity(city: String){
+        _cities.remove(city)
+    }
+}
